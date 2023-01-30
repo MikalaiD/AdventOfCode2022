f2e141360c222f8ebe82b862bf0ba2021b8e95b7
DESKTOP-4928LN9\dudko <dudko.na@gmail.com>
2023-01-30 18:44:49 +0100

    day 9 uses "teleportation" which is not good since the tail is moving in less efficient trajectory in reality

--- src/main/java/org/example/Day09RopeBridge.java
+++ src/main/java/org/example/Day09RopeBridge.java
@@ -31,7 +31,6 @@ public class Day09RopeBridge {
     private static Knot head = new Knot(0, 0);
     private static LinkedList<Knot> knots = new LinkedList<>();
     private Set<Point> visitedByTail = new HashSet<>();
-    private Point lastTailPosition = new Point(0,0);
 
     public Rope(int length) {
       for (int i = 0; i < length; i++) {
@@ -40,7 +39,7 @@ public class Day09RopeBridge {
           knots.getLast().setTail(true);
         }
       }
-      visitedByTail.add(lastTailPosition);
+      visitedByTail.add(new Point(0, 0));
     }
 
     public static Knot getHead() {
@@ -53,8 +52,14 @@ public class Day09RopeBridge {
 
     public static class Knot extends Point {
 
+
       public Knot(int x, int y) {
         super(x, y);
+      }
+
+      public Knot(Knot p) {
+        super(p);
+        this.tail = p.tail;
       }
 
       private boolean tail = false;
@@ -69,7 +74,6 @@ public class Day09RopeBridge {
     }
 
 
-
     //########################################################
     private void saveTailPoint(Point currentPoint) {
       visitedByTail.add(new Point(currentPoint));
@@ -79,37 +83,54 @@ public class Day09RopeBridge {
       var currentHead = head;
       head.move(getXHeadChange(move), getYHeadChange(move));
 //      System.out.println("Head new position: " + positionHead);
-      for(Knot knot : knots) {
-        if (areNearby(currentHead, knot)) break;
+      for (Knot knot : knots) {
+        if (areNearby(currentHead, knot)) {
+          break;
+        }
         moveKnot(currentHead, knot);
-        currentHead=knot;
+        currentHead = knot;
       }
-      print(move, 35);
+      print(move, 30);
     }
 
     private boolean areNearby(Knot head, Knot tail) {
-      return Math.abs(head.x - tail.x)<=1 && Math.abs(head.y - tail.y)<=1;
+      return Math.abs(head.x - tail.x) <= 1 && Math.abs(head.y - tail.y) <= 1;
     }
 
     private void moveKnot(final Knot head, final Knot knot) {
+      var initial = new Knot(knot);
       var absDistX = Math.abs(head.x - knot.x);
       var absDistY = Math.abs(head.y - knot.y);
       var dirX = absDistX == 0 ? 0 : (head.x - knot.x) / absDistX;
       var dirY = absDistY == 0 ? 0 : (head.y - knot.y) / absDistY;
-
-      if(knot.isTail()) {
-
+      if (absDistX < absDistY) {
+        knot.translate(dirX * absDistX, (absDistY - 1) * dirY);
+      } else if (absDistX > absDistY) {
+        knot.translate((absDistX - 1) * dirX, dirY * absDistY);
       } else {
-        if (absDistX < absDistY) {
-          knot.translate(dirX * absDistX, (absDistY - 1) * dirY);
-        } else if (absDistX > absDistY) {
-          knot.translate((absDistX - 1) * dirX, dirY * absDistY);
-        } else {
-          knot.translate((absDistX - 1) * dirX, (absDistY - 1) * dirY);
-        }
+        knot.translate((absDistX - 1) * dirX, (absDistY - 1) * dirY);
+      }
+
+      if (knot.isTail()) {
+        trackEachMoveBetween(knot, initial);
       }
     }
 
+    private void trackEachMoveBetween(final Knot head, final Knot tail) {
+      var absDistX = Math.abs(head.x - tail.x);
+      var absDistY = Math.abs(head.y - tail.y);
+      var dirX = absDistX == 0 ? 0 : (head.x - tail.x) / absDistX;
+      var dirY = absDistY == 0 ? 0 : (head.y - tail.y) / absDistY;
+      while (absDistX >= 1 || absDistY >= 1) {
+        tail.translate(dirX, dirY);
+        visitedByTail.add(new Point(tail));
+        System.out.println("tail visited point: " + tail);
+        absDistX = Math.abs(head.x - tail.x);
+        absDistY = Math.abs(head.y - tail.y);
+        dirX = absDistX == 0 ? 0 : (head.x - tail.x) / absDistX;
+        dirY = absDistY == 0 ? 0 : (head.y - tail.y) / absDistY;
+      }
+    }
 
 
     private int getYHeadChange(String move) {
@@ -135,19 +156,22 @@ public class Day09RopeBridge {
     }
   }
 
-  private static void print(String move, int fieldSize){
+  private static void print(String move, int fieldSize) {
     System.out.println("############## Move " + move + "#############");
     List<List<String>> matrix = IntStream.range(0, fieldSize)
-        .mapToObj(row -> IntStream.range(0, fieldSize).mapToObj(i -> ".").collect(Collectors.toList())).collect(
+        .mapToObj(
+            row -> IntStream.range(0, fieldSize).mapToObj(i -> ".").collect(Collectors.toList()))
+        .collect(
             Collectors.toList());
-    var adj = fieldSize/2;
-    matrix.get(Rope.getHead().y+adj).set(Rope.getHead().x+adj, "H");
-    for (int i = 0; i<Rope.getKnots().size();i++ ) {
+    var adj = fieldSize / 2;
+    matrix.get(Rope.getHead().y + adj).set(Rope.getHead().x + adj, "H");
+    for (int i = 0; i < Rope.getKnots().size(); i++) {
       Knot knot = Rope.getKnots().get(i);
-      matrix.get(knot.y+adj).set(knot.x+adj, String.valueOf(i+1));
+      matrix.get(knot.y + adj).set(knot.x + adj, String.valueOf(i + 1));
     }
+    matrix.get(adj).set(adj, "s");
     Collections.reverse(matrix);
-    matrix.stream().map(list-> String.join("", list)).forEach(System.out::println);
+    matrix.stream().map(list -> String.join("", list)).forEach(System.out::println);
   }
 }
 
