e55ea826866289352021c5baaf96939b9cfbbc29
DESKTOP-4928LN9\dudko <dudko.na@gmail.com>
2023-08-19 15:53:12 +0200

    refactored - added memo

--- src/main/java/org/algo/spoj/dynamic/ACODE.java
+++ src/main/java/org/algo/spoj/dynamic/ACODE.java
@@ -1,11 +1,14 @@
 package org.algo.spoj.dynamic;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class ACODE {
+
+  final static HashMap<Integer, Long> memo = new HashMap<>();
 
   public static void main(String[] args) throws java.lang.Exception {
     Scanner scanner = new Scanner(System.in);
@@ -25,7 +28,7 @@ public class ACODE {
     int j = 0;
     for (int i = 0; i < list.size();) {
       if (i == list.size() - 1 || getEncoded(list, i) > 26 || getEncoded(list, i) == 10) {
-        output *= customFibonacci(i - j + 1);
+        output *= getOrCalculatePermutations(i - j + 1);
         if (i != list.size() - 1 && getEncoded(list, i) == 10) {
           j = i + 2;
           i++;
@@ -36,35 +39,18 @@ public class ACODE {
       i++;
     }
     return output;
-//    if (list.size()-offset==1) {
-//      return 1;
-//    } else if(list.size()==offset){
-//      return 0;
-//    }
-//    var encoded = list.get(offset)*10+list.get(offset+1);
-//    if (memo[offset]!=0) {
-//      return memo[offset];
-//    }
-//    if (encoded == 10) {
-//      long subVal = traverse(list, offset + 2);
-//      memo[offset]=subVal;
-//      return subVal;
-//    } else if (encoded > 26) {
-//      long subVal = traverse(list, offset + 1);
-//      memo[offset]=subVal;
-//      return subVal;
-//    } else {
-//      long subVal = traverse(list, offset + 1) + traverse(list, offset + 2);
-//      memo[offset]=subVal;
-//      return subVal;
-//    }
   }
 
   private static int getEncoded(List<Integer> list, int i) {
     return list.get(i) * 10 + list.get(i + 1);
   }
 
-  private static long customFibonacci(int size) {
+  private static long getOrCalculatePermutations(int size) {
+    memo.putIfAbsent(size, calculateCustomFibonacciFor(size));
+    return memo.get(size);
+  }
+
+  private static long calculateCustomFibonacciFor(int size) {
     var output = 1L;
     var f1 = 1L;
     var f2 = 1L;
