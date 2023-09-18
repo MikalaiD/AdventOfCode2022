f420958c06bf8732e71c3a960ac4e238389542c9
DESKTOP-4928LN9\dudko <dudko.na@gmail.com>
2023-09-18 19:39:40 +0200

    6000 in 1 sec, still not good enough, need to optimise

--- src/main/java/org/algo/spoj/dynamic/AIBOHP.java
+++ src/main/java/org/algo/spoj/dynamic/AIBOHP.java
@@ -13,38 +13,61 @@ public class AIBOHP {
     var cases = in.nextInt();
     while (in.hasNext() && cases != 0) {
       final var input = in.nextLine();
-      if(input.isEmpty()) continue;
-      int output = solve(input, 0, input.length() - 1);
+      if (input.isEmpty()) continue;
+      int output = solve2(input);
       System.out.println(output);
       cases--;
     }
   }
-
-  public static int solve(final String input,
-      final int offsetLeft,
-      final int offsetRight) {
-    if((offsetRight==offsetLeft)){
+  //stack overflow here with 6000 letters if used purely
+  public static int solve(final String input, final int offsetLeft, final int offsetRight) {
+    if ((offsetRight == offsetLeft)) {
       return 0;
     }
     var leftChar = input.charAt(offsetLeft);
     var rightChar = input.charAt(offsetRight);
-    if(offsetRight-offsetLeft==1 && leftChar == rightChar){
-      return 0;
-    }
-    if (offsetRight-offsetLeft==1){
-      return 1;
+    if (offsetRight - offsetLeft == 1 ) {
+      return  leftChar == rightChar ? 0 : 1;
     }
     final var key = input.substring(offsetLeft, offsetRight + 1);
+//    final var key = normalise(input.substring(offsetLeft, offsetRight + 1));
     if (memo.containsKey(key)) {
       return memo.get(key);
     }
     if (leftChar == rightChar) {
-      memo.put(key, solve(input, offsetLeft + 1, offsetRight - 1));
-      return memo.get(key);
+      return solve(input, offsetLeft + 1, offsetRight - 1);
     } else {
-      memo.put(key, Math.min(solve(input, offsetLeft + 1, offsetRight),
-          solve(input, offsetLeft, offsetRight - 1))+1);
+      memo.put(
+          key,
+          Math.min(
+                  solve(input, offsetLeft + 1, offsetRight),
+                  solve(input, offsetLeft, offsetRight - 1))
+              + 1);
       return memo.get(key);
     }
+  }
+
+  public static int solve2(final String input){
+    for(int i = 1; i<=input.length(); i++){
+      var key = input.substring(0, i);
+      var value = solve(key, 0, key.length()-1);
+      memo.put(key, value);
+    }
+    return memo.get(input);
+  }
+
+  private static String normalise(final String input) {
+    var normalizedLetter = 'a';
+    var output = new StringBuilder();
+    Map<Character, Character> normalisationMap = new HashMap<>();
+    for (int i = 0; i < input.length(); i++) {
+      char oldLetter = input.charAt(i);
+      if(!normalisationMap.containsKey(oldLetter)){
+        normalisationMap.put(oldLetter,normalizedLetter);
+        normalizedLetter++;
+      }
+      output.append(normalisationMap.get(input.charAt(i)));
+    }
+    return output.toString();
   }
 }
--- src/test/java/org/algo/spoj/dynamic/AIBOHPTest.java
+++ src/test/java/org/algo/spoj/dynamic/AIBOHPTest.java
@@ -2,7 +2,15 @@ package org.algo.spoj.dynamic;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.HashMap;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -11,19 +19,42 @@ class AIBOHPTest {
 
   @ParameterizedTest
   @MethodSource("provideScenarios")
-  void noChangeNeeded(String input, int expected){
-    int actual = AIBOHP.solve(input, 0, input.length() - 1);
+  void noChangeNeeded(String input, int expected) {
+    Instant start = Instant.now();
+    int actual = AIBOHP.solve2(input);
+    Instant finish = Instant.now();
+    long timeElapsed = Duration.between(start, finish).toMillis();
+    System.out.println("Time elapsed: " + timeElapsed);
     assertEquals(expected, actual);
   }
 
-  private static Stream<Arguments> provideScenarios(){
+  @Test
+  void testNormalisation (){
+    AIBOHP.memo = new HashMap<>();
+    var inputA = "abbc";
+    var inputB = "nggt";
+    var inputC = "xppr";
+
+    AIBOHP.solve(inputA, 0, inputA.length()-1);
+    AIBOHP.solve(inputB, 0, inputB.length()-1);
+    AIBOHP.solve(inputC, 0, inputC.length()-1);
+
+    assertEquals(3, AIBOHP.memo.entrySet().size());
+  }
+
+  private static Stream<Arguments> provideScenarios() throws IOException {
     return Stream.of(
-        Arguments.of("a", 0),
-        Arguments.of("aba", 0),
-        Arguments.of("aa", 0),
-        Arguments.of("aab", 1),
-        Arguments.of("ababbac", 2),
-        Arguments.of("ababbacbbacabbaababbabbaabacbcabvbababbccabbaccabbccbebaaaaababbababababababababbabababaabababbabbababbabjhsdbababbabbabbssjhbbbababsjdasjjjasdjajjjabbakhskahskhadkahsdkjhaskdhakjdhaskjhdkashdkashdkashdkashdaskhda", 109)
+//        Arguments.of("a", 0),
+//        Arguments.of("aba", 0),
+//        Arguments.of("aa", 0),
+//        Arguments.of("aab", 1),
+//        Arguments.of("ababbac", 2),
+//        Arguments.of(
+//            "ababbacbbacabbaababbabbaabacababbacbbacabbaababbabbaabacbcabvbababbccabbaccabbccbebaaaaababbabababababcabvbababbccabbaccabbccbebaaaaababbababababaababbacbbacabbaababbabbaabacbcabvbababbccabbaccabbccbebaaaaababbababababaababbacbbacabbaababbabbaabacbcabvbababbccabbaccabbccbebaaaaababbababababababababbabababaabababbabbababbabjhsdbababbabbabbssjhbbbababsjdasjjjasdjajjjabbakhskahskhadkahsdkjhaskdhakjdhaskjhdkashdkashdkashdkashdaskhda",
+//            154),
+        Arguments.of(
+            Files.lines(Path.of("src/test/resources/spoj/AIBOHPT/6000.txt")).collect(Collectors.joining()),
+            365)
     );
   }
 }
